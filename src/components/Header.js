857c572b539718407fa93d7620d946ac3416b8d8
mattie236d <mattie236mitchell@outlook.com>
2025-09-01 15:34:56 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1>HHA Exchange</h1>
+      <p>Your solution for home health aide expense management.</p>
+    </header>
+  );
+};
+
+export default Header;
